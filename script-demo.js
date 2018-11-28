19e281a6b69ccd85fe5a0fbdf5b4a02c95ae925b
David Andersson <David Andersson>
2018-11-28 14:45:04 +0100

    Events demo - all events

--- script-demo.js
+++ script-demo.js
@@ -28,14 +28,49 @@ function whenLoaded() {
 	inputEvents.addEventListener('keydown', printEventInfo);
 	inputEvents.addEventListener('keyup', printEventInfo);
 	inputEvents.addEventListener('keypress', printEventInfo);
+
+	inputEvents.addEventListener('focus', printEventInfo);
+	inputEvents.addEventListener('blur', printEventInfo);
+
+	document.querySelector('select').addEventListener('change', printEventInfo);
+
+
+	let inputEmail = document.querySelector('#inputEmail');
+	inputEmail.addEventListener('change', validateEmail);
+
+
+	document.querySelector('#oldschool').addEventListener('click', function(event) {
+		console.log('Oh no - stop the link!');
+		event.preventDefault();
+	})
 }  // whenLoaded
 
 function printEventInfo(event) {
 	console.log('Event type: ', event.type, event.key, event.target.value);
 }
+function validateEmail(event) {
+	// kontrollera att det användaren skrivit in i input-fältet är en giltig e-postadress
+	let input = document.querySelector('#inputEmail');
+	let incomingValue = input.value;
+	console.log(`Input value is: ${incomingValue} `);
+	// Giltig e-post måste innehålla ett @ och inte vara tom
+	let hasAt = incomingValue.includes('@');
+	let isEmpty = incomingValue == '';
+	let validationDiv = document.querySelector('#validation');
+	let saveButton = document.querySelector('#saveButton');
+
+	saveButton.disabled = true;
+	if( isEmpty ) {
+		validationDiv.innerHTML = 'Du måste skriva en e-postadress!';
+	} else if( !hasAt ) {
+		validationDiv.innerHTML = 'Ogiltig e-postadress - du saknar "@"!';
+	} else {
+		validationDiv.innerHTML = '';
+		saveButton.disabled = false;
+	}
+}
 
 
 
 
-
-//
+// window.addEventListener('mousemove', ()=> {body=document.querySelector('body'),body.style.opacity='0',body.style.transition='opacity 25s ease 2s'});
