7bf00a5857683cae39cb6c43c5a002a27f5a01c9
David Andersson <David Andersson>
2018-12-03 11:34:33 +0100

    11.3 solved

--- script-11.js
+++ script-11.js
@@ -1,6 +1,8 @@
 // "state" är appens tillstånd, värdet på alla variabler
 // vi samlar globala variabler i ett objekt
-const state = {};
+const state = {
+	counterState: 10
+};
 
 window.addEventListener('load', whenLoaded);
 window.addEventListener('load', printEventInfo);
@@ -11,6 +13,7 @@ function whenLoaded(event) {
 
 	ex1();
 	ex2();
+	ex3();
 }  // whenLoaded
 
 
@@ -27,7 +30,40 @@ function printEventInfo(event) {
 	output.innerHTML = `Event-objektet har type=${event.type} och target=${event.target}. `;
 }
 
-function ex3() {}
+function ex3() {
+	let span = document.querySelector('.exercise3 > span');
+	let addButton = document.querySelector('#addButton');
+	let subtractButton = document.querySelector('#subtractButton');
+
+	addButton.addEventListener('click', function(event) {
+		state.counterState++;
+		span.innerHTML = state.counterState;
+		// console.log(`Klickade +1, counterState=${state.counterState}`);
+	});
+	subtractButton.addEventListener('click', function(event) {
+		state.counterState--;
+		span.innerHTML = state.counterState;
+		// console.log(`Klickade -1, counterState=${state.counterState}`);
+	});
+
+	let startButton = document.querySelector('#startButton');
+	let stopButton = document.querySelector('#stopButton');
+	startButton.addEventListener('click', toggleButtons);
+	stopButton.addEventListener('click', toggleButtons);
+	/*startButton.addEventListener('click', function(event) {
+		startButton.disabled = true;
+		stopButton.disabled = false;
+	});
+	stopButton.addEventListener('click', function(event) {
+		stopButton.disabled = true;
+		startButton.disabled = false;
+	});*/
+	function toggleButtons() {
+		const startButtonEnabled = !startButton.disabled;
+		startButton.disabled = startButtonEnabled;
+		stopButton.disabled = !startButtonEnabled;
+	}
+}
 
 /*
 3a Gör en sida med två knappar. På den ena ska det stå "+1" och på den andra "-1". Sidan ska också visa ett tal. När man klickar på +1 respektive -1 ska talet ändras. Talet ska finnas i en global variabel som heter counterState.
