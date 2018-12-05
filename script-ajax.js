9af46a3bc8d69c525923ce837e7585d634349c2f
David Andersson <David Andersson>
2018-12-05 16:57:58 +0100

    Ajax demo - star wars

--- script-ajax.js
+++ script-ajax.js
@@ -13,6 +13,31 @@ $(document).ready(function() {
 		.fail(onError)
 		.always(onComplete);
 	})
+
+	// leta upp knappen ur DOM
+	// lägg till en klickhändelse:
+	// - skapa AJAX GET request, med rätt querystring/url och settings
+	// skapa funktion för att hantera svaret från servern:
+	// - gör om strängen till ett JS objekt
+	// - visa information från objektet på webbsidan
+	$('#swButton').on('click', event => {
+		const url = 'https://swapi.co/api/people';
+		const settings = {
+			method: 'GET'
+			// data: - det här API:et använder inte querystring
+		}
+		let person = $('#swPerson').val();
+		let finalUrl = url + '/' + person;
+		console.log('Engaging warp speed! url=' + finalUrl);
+		$.ajax(finalUrl, settings)
+		.done(data => {
+			// data är redan ett objekt, vi behöver inte använda JSON.parse
+			console.log('parsed data: ', data);
+			$('.swResult').html(`This is ${data.name} from ${data.homeworld}.`);
+			// Förbättringspotential: homeworld är en API-länk. Vi kan hämta information om planeten med ett till AJAX GET request...
+		})
+	})
+
 })  // jQuery document.ready
 
 function whenAjaxDone(data) {
